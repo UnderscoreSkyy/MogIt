1f3451a6dd864ea85854a2eb555ddf4507eca592
Skyy <underscore@skyy.com>
2021-02-28 19:53:40 +0100

    Initialisation

---
+++ src/main/java/underscore/skyy/mogit/Mogit.java
@@ -0,0 +1,10 @@
+package underscore.skyy.mogit;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mogit implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
